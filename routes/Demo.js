07c9a065a7b4dbd347df0477057e299f9374ad19
Nikhil Jaunjal <31154769+nikhil-jaunjal@users.noreply.github.com>
2019-02-28 15:40:59 +0530

    Add files via upload

--- routes/Demo.js
+++ routes/Demo.js
@@ -3,14 +3,7 @@ const router = express.Router();
 const user = require('../user');
 
 router.post('/', function(req,res,next){
-    var userdata = new user(
-		{ firstName :req.body.firstName,
-		  lastName: req.body.lastName,
-		  email : req.body.email
-		});
-	userdata.save(function (err, data) {
-		res.send(data);
-	});
+	res.send(req.body);
 });
 
 module.exports = router;
